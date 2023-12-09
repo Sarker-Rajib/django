4c3d72154d4d95e95c55b23ec5de773bb44e98fe
Rajib Sarker <redhat0593@gmail.com>
2023-12-09 12:42:25 +0600

    model set up done

--- 04.django-forms/04.blog_project/Post/models.py
+++ 04.django-forms/04.blog_project/Post/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from categories.models import Category
+from Author.models import Author
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=35)
+    content = models.TextField()
+    category = models.ManyToManyField(Category)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
