be2cc95bd34ebe1da417f9090f58cc572ea1859f
Rajib Sarker <redhat0593@gmail.com>
2023-12-13 01:11:11 +0600

    assignment done

--- 04.django-forms/task_management/Task/urls.py
+++ 04.django-forms/task_management/Task/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import add_task, taskMenu
+from .views import add_task, taskMenu, edit_task, delete_task
 
 urlpatterns = [
     path('', add_task, name='add-task'),
     path('tasks', taskMenu, name='taskmenu'),
+    path('edit/<int:id>', edit_task, name='edit-task'),
+    path('delete/<int:id>', delete_task, name='delete-task'),
 ]
--- 04.django-forms/task_management/Task/views.py
+++ 04.django-forms/task_management/Task/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import Task_Form
+from .models import TaskModel
 
 # Create your views here.
 def add_task(request):
+    if request.method == 'POST':
+        form = Task_Form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('taskmenu')
+    else:
+        form = Task_Form()
+    
     return render(request, 'task/add-task.html', {'form': Task_Form})
 
 def taskMenu(request):
-    return render(request, 'task/task-menu.html')
+    data = TaskModel.objects.all()
+    return render(request, 'task/task-menu.html', {'data': data})
+
+
+def edit_task(request, id):
+    task = TaskModel.objects.get(pk=id)
+    if request.method == 'POST':
+        form = Task_Form(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('taskmenu')
+    else:
+        form = Task_Form(instance=task)
+    
+    return render(request, 'task/edit-task.html', {'form': form})
+
+def delete_task(request, id):
+    print(request)
+    task = TaskModel.objects.get(pk=id)
+    task.delete()
+    return redirect('taskmenu')
