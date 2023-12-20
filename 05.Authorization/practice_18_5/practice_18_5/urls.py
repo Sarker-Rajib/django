875fe4d3612062b74a7a9c1fabc4e39cddb4c696
Rajib Sarker <redhat0593@gmail.com>
2023-12-21 02:41:16 +0600

    home done

--- 05.Authorization/practice_18_5/practice_18_5/urls.py
+++ 05.Authorization/practice_18_5/practice_18_5/urls.py
@@ -4,6 +4,7 @@ from .views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
+    path('category/<slug:category_slug>/', index, name='post_by_category'),
     path('user/', include('User.urls')),
     path('post/', include('Posts.urls')),
     path('categoty/', include('Category.urls')),
