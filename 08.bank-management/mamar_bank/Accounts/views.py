712bc9d9b1c47e13d131ba4e80b8803fc3ddd20f
Rajib Sarker <redhat0593@gmail.com>
2023-12-26 23:55:10 +0600

    login logout added

--- 08.bank-management/mamar_bank/Accounts/views.py
+++ 08.bank-management/mamar_bank/Accounts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import FormView
 from django.urls import reverse_lazy
-from django.contrib.auth import login
-from .forms import UserRegistrationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views import View
+from .forms import UserRegistrationForm, UserUpdateForm
 
 # Create your views here.
 class UserRegistrationView(FormView):
@@ -19,3 +21,29 @@ class UserRegistrationView(FormView):
     def form_invalid(self, form):
         print("Form is invalid.")
         return super().form_invalid(form)
+    
+class UserLoginView(LoginView):
+    template_name = 'accounts/user_login.html'
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+class UserLogoutView(LogoutView):
+    def get_success_url(self):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+        return reverse_lazy('home')
+
+class UserBankAccountUpdateView(View):
+    template_name = 'accounts/profile.html'
+
+    def get(self, request):
+        form = UserUpdateForm(instance=request.user)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')  # Redirect to the user's profile page
+        return render(request, self.template_name, {'form': form})
+    
